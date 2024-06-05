4e2ea5a15417c8af793b7b37613d84acf77dad12
Denzel Elden Wijaya <DENZ0006@e.ntu.edu.sg>
2024-06-05 15:32:22 +0800

    editing scene1

--- Assets/Scripts/GameStartMenu.cs
+++ Assets/Scripts/GameStartMenu.cs
@@ -7,14 +7,11 @@ public class GameStartMenu : MonoBehaviour
 {
     [Header("UI Pages")]
     public GameObject mainMenu;
-    public GameObject options;
     public GameObject about;
 
     [Header("Main Menu Buttons")]
     public Button startButton;
-    public Button optionButton;
     public Button aboutButton;
-    public Button quitButton;
 
     public List<Button> returnButtons;
 
@@ -25,9 +22,7 @@ public class GameStartMenu : MonoBehaviour
 
         //Hook events
         startButton.onClick.AddListener(StartGame);
-        optionButton.onClick.AddListener(EnableOption);
         aboutButton.onClick.AddListener(EnableAbout);
-        quitButton.onClick.AddListener(QuitGame);
 
         foreach (var item in returnButtons)
         {
@@ -49,26 +44,22 @@ public class GameStartMenu : MonoBehaviour
     public void HideAll()
     {
         mainMenu.SetActive(false);
-        options.SetActive(false);
         about.SetActive(false);
     }
 
     public void EnableMainMenu()
     {
         mainMenu.SetActive(true);
-        options.SetActive(false);
         about.SetActive(false);
     }
     public void EnableOption()
     {
         mainMenu.SetActive(false);
-        options.SetActive(true);
         about.SetActive(false);
     }
     public void EnableAbout()
     {
         mainMenu.SetActive(false);
-        options.SetActive(false);
         about.SetActive(true);
     }
 }

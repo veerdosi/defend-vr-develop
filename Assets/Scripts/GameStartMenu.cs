94d89858527a6fe82513f1bb07ff6293d6ded588
Veer Dosi <veerdosi00@gmail.com>
2024-06-06 15:05:08 +0800

    updated about scene back button

--- Assets/Scripts/GameStartMenu.cs
+++ Assets/Scripts/GameStartMenu.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameStartMenu : MonoBehaviour
 {
     [Header("UI Pages")]
     public GameObject mainMenu;
-    public GameObject options;
     public GameObject about;
 
     [Header("Main Menu Buttons")]
     public Button startButton;
-    public Button optionButton;
     public Button aboutButton;
-    public Button quitButton;
 
     public List<Button> returnButtons;
 
@@ -25,9 +23,7 @@ public class GameStartMenu : MonoBehaviour
 
         //Hook events
         startButton.onClick.AddListener(StartGame);
-        optionButton.onClick.AddListener(EnableOption);
         aboutButton.onClick.AddListener(EnableAbout);
-        quitButton.onClick.AddListener(QuitGame);
 
         foreach (var item in returnButtons)
         {
@@ -35,40 +31,27 @@ public class GameStartMenu : MonoBehaviour
         }
     }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
 
     public void StartGame()
     {
         HideAll();
-        SceneTransitionManager.singleton.GoToSceneAsync(1);
+        SceneManager.LoadScene(1);
     }
 
     public void HideAll()
     {
         mainMenu.SetActive(false);
-        options.SetActive(false);
         about.SetActive(false);
     }
 
     public void EnableMainMenu()
     {
         mainMenu.SetActive(true);
-        options.SetActive(false);
-        about.SetActive(false);
-    }
-    public void EnableOption()
-    {
-        mainMenu.SetActive(false);
-        options.SetActive(true);
         about.SetActive(false);
     }
     public void EnableAbout()
     {
         mainMenu.SetActive(false);
-        options.SetActive(false);
         about.SetActive(true);
     }
 }

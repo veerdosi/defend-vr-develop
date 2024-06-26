e8fa11d83db4b2d90024d034a9e14eb4ac9a5b04
Denzel Elden Wijaya <DENZ0006@e.ntu.edu.sg>
2024-06-26 16:48:01 +0800

    adding a fade scree

---
+++ Assets/sceneTransitionmanager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTransitionmanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public FadeScreen1 fadeScreen;
+
+    public void GoToScene(string sceneName)
+    {
+        StartCoroutine(GoToSceneRoutine(sceneName));
+    }
+
+    IEnumerator GoToSceneRoutine(string sceneName)
+    {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.fadeDuration);
+        SceneManager.LoadScene(sceneName);
+    }
+}
